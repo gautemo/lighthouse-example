464b582654f09aa1faed94c359491275e529e334
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-11-01 22:03:14 +0100

    init vite project

--- vanilla/app.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('.counter button')
-const countEl = document.querySelector('#count')
-let count = 0
-button.addEventListener('click', () => {
-  count++
-  countEl.innerHTML = count
-})
