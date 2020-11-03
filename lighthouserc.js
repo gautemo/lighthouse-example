24f0cf75dc80777604f00d21d86329849e4fd4f6
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-11-03 20:04:43 +0100

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        "categories:performance": ["warn", { "minScore": 0.9 }],
+        "categories:performance": ["warn", { "minScore": 1 }],
         "categories:accessibility": ["error", { "minScore": 1 }]
       }
     }
