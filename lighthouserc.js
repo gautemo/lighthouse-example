635643262e3aa6833e9f376f43de2c286c680dbb
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-11-04 21:36:11 +0100

    updated lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,17 @@
 module.exports = {
   ci: {
     collect: {
+      staticDistDir: 'dist',
       isSinglePageApplication: true,
-      url: ['/', '/about', '404']
+      url: ['', 'about', '404']
     },
     assert: {
+      preset: "lighthouse:no-pwa",
       assertions: {
-        "categories:performance": ["warn", { "minScore": 1 }],
         "categories:performance": ["error", { "minScore": 0.9 }],
-        "categories:accessibility": ["error", { "minScore": 1 }]
+        "categories:accessibility": ["error", { "minScore": 1 }],
+        "meta-description": "off",
+        "robots-txt": "off",
       }
     },
     upload: {
