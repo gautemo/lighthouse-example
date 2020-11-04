9cb25c6853f4bb087e321e35366bbcfd578086d0
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-11-04 21:40:39 +0100

    loop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,24 @@
 <template>
   <h1>Yo</h1>
-  <p>You made me loop <span>{{times}}</span> times!</p>
+  <p>
+    You made me loop <span>{{ times }}</span> times!
+  </p>
 </template>
 
 <script>
-export default {
-  setup(){
-    let times = 0;
-    for(let i=0; i<5000; i++){
-      times++
-    }
-    return { times }
-  }
+let times = 0;
+for (let i = 0; i < 5000; i++) {
+  times++;
 }
+export default {
+  setup() {
+    return { times };
+  },
+};
 </script>
 
 <style scoped>
-span{
+span {
   font-size: 3em;
   font-weight: bold;
   color: rgb(110, 0, 0);
